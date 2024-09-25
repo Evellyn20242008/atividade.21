ece7169abdebb168c93181326c21dca368f0d0f7
Evellyn20242008 <Evellynmartins579@gmail.com>
2024-09-25 13:29:41 -0300

    ativ21

---
+++ ativ21.py
@@ -0,0 +1,23 @@
+# Crie um programa que utilize uma estrutura de repetição para somar todos os números pares de 1 a 100 e exiba o resultado.
+soma = 0
+
+# Loop para iterar pelos números de 1 a 100git init 
+for i in range(1, 101):
+    # Verifica se o número é par
+    if i % 2 == 0:
+        soma += i  # Adiciona o número par à soma
+
+# Exibe o resultado
+print(f"A soma dos números pares de 1 a 100 é: {soma}")
+
+ Crie um programa que utilize uma estrutura de repetição para somar todos os números pares de 1 a 100 e exiba o resultado.
+soma = 0
+
+# Loop para iterar pelos números de 1 a 100git init 
+for i in range(1, 101):
+    # Verifica se o número é par
+    if i % 2 == 0:
+        soma += i  # Adiciona o número par à soma
+
+# Exibe o resultado
+print(f"A soma dos números pares de 1 a 100 é: {soma}")
